1939b1b6a93931c1e831bb2082fd23f98978469d
Bohdan Zhuravel <bohdan@zhuravel.bz>
2015-02-12 18:02:14 +0200

    Initial commit

---
+++ app/code/community/Talkable/CheckoutOffer/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Talkable CheckoutOffer for Magento
+ *
+ * @package     Talkable_CheckoutOffer
+ * @author      Talkable (http://www.talkable.com/)
+ * @copyright   Copyright (c) 2015 Talkable (http://www.talkable.com/)
+ * @license     MIT
+ */
+
+class Talkable_CheckoutOffer_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
