7489c0bfc6182159b476c01374c06c642acbbee2
Bohdan Zhuravel <bohdan@zhuravel.bz>
2015-02-13 16:47:12 +0200

    Multishipping checkout

--- app/code/community/Talkable/CheckoutOffer/Block/Checkoutoffer.php
+++ app/code/community/Talkable/CheckoutOffer/Block/Checkoutoffer.php
@@ -11,42 +11,11 @@
 class Talkable_CheckoutOffer_Block_Checkoutoffer extends Mage_Checkout_Block_Onepage_Success
 {
 
-    public function getSiteId()
+    public function getCheckoutOrder()
     {
-        return trim(Mage::getStoreConfig("checkoutoffer/settings/site_id"));
-    }
-
-    public function getCampaignTags()
-    {
-        return array_filter(array_map("trim", explode(",", Mage::getStoreConfig("checkoutoffer/settings/campaign_tags"))));
-    }
-
-    public function getOrderData()
-    {
-        $order = Mage::getModel("sales/order")->loadByIncrementId($this->getOrderId());
-
-        $retval = array(
-            "order_number" => $order->getIncrementId(),
-            "order_date"   => $order->getCreatedAt(),
-            "email"        => $order->getCustomerEmail(),
-            "subtotal"     => $order->getSubtotal(),
-            "coupon_code"  => $order->getCouponCode(),
-            "customer_id"  => $order->getCustomerId(),
-            "items"        => array(),
-            "first_name"   => $order->getCustomerFirstname(),
-            "last_name"    => $order->getCustomerLastname(),
-        );
-
-        foreach ($order->getAllVisibleItems() as $product) {
-            $retval["items"][] = array(
-                "product_id" => $product->getSku(),
-                "price"      => $product->getPrice(),
-                "quantity"   => $product->getQtyOrdered(),
-                "title"      => $product->getName(),
-            );
+        if ($this->getOrderId()) {
+            return Mage::getModel("sales/order")->loadByIncrementId($this->getOrderId());
         }
-
-        return $retval;
     }
 
 }
